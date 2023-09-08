3e8b324824c6d03642194f243de3fa174528aa3a
sand2104 <prasadsandy2000@gmail.com>
2023-09-08 22:31:06 +0530

    first commit

---
+++ EcommerceSite/EcommerceSite/Models/Laptop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EcommerceSite.Models
+{
+    [Table("Laptop")]
+    public class Laptop
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Company { get;set; }
+        public  string ModelName { get;set; }
+        public double Price { get;set; }
+    }
+}
